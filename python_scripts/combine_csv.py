0edd0ed268b60246e9ea8e67e29a4416bd203e98
Balabalu-Ve <60904972+Balabalu-VE@users.noreply.github.com>
2024-05-01 09:04:26 -0500

    included new stocks

--- python_scripts/combine_csv.py
+++ python_scripts/combine_csv.py
@@ -14,15 +14,27 @@ def merge_csv_files(input_folders, output_file):
                     reader = csv.reader(infile)
                     next(reader)  # Skip header
                     filename_without_extension = os.path.splitext(csv_file)[0]
-
+                    count = 0
                     # Generate a random number between 0 and 10 for each unique StockName
                     random_holdings = {filename_without_extension: random.uniform(0, 10)}
-
                     for row in reader:
+                        if count < 1:
+                            count = count + 1 
+                            divider = float(row[4]) * .2
+                            if divider < 1:
+                                divider = divider * 10
+                            elif divider > 5:
+                                divider = divider / 10
+                            print(filename_without_extension)
+                            print(float(divider))
+
                         # Selecting columns StockType, StockName, Date, Close, and Value
                         stock_name = filename_without_extension
-                        holdings = random_holdings.get(stock_name, random.uniform(0, 10))
-                        value = float(row[4]) * holdings 
+                        holdings = random_holdings.get(stock_name, random.uniform(0, 5))
+                        
+  
+                        value = float(row[4]) / divider #* (holdings / divider)
+                      
                         selected_row = [folder_type, stock_name, row[0], row[4], holdings, value]
                         writer.writerow(selected_row)
 
@@ -33,3 +45,9 @@ input_folders = {
 output_file = 'output.csv'
 
 merge_csv_files(input_folders, output_file)
+
+# Execute file1.py
+exec(open('python_scripts/filter.py').read())
+
+# Execute file2.py
+exec(open('python_scripts/addPercentage.py').read())
